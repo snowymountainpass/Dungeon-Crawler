96940c295857bea9d4e65a2be3c1b7d19887c206
LA Popa <laura_stanescu_1991@yahoo.com>
2021-12-09 13:43:53 +0200

    [wip] player loading

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDao.java
@@ -10,5 +10,6 @@ public interface PlayerDao {
     void add(PlayerModel player);
     void update(PlayerModel player) throws SQLException;
     PlayerModel get(int id);
+    PlayerModel get(String name);
     ArrayList<String> getAll();
 }
