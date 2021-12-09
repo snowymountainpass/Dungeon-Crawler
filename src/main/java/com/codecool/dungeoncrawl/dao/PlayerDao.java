3e93b485951856c9d4e01fdff76c97ac33f38d5f
LA Popa <laura_stanescu_1991@yahoo.com>
2021-12-09 11:57:16 +0200

    implemented method in playerDAO, get all saved player names

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDao.java
@@ -3,11 +3,12 @@ package com.codecool.dungeoncrawl.dao;
 import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface PlayerDao {
     void add(PlayerModel player);
     void update(PlayerModel player) throws SQLException;
     PlayerModel get(int id);
-    List<PlayerModel> getAll();
+    ArrayList<String> getAll();
 }
